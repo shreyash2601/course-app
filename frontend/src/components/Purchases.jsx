885ab82bb6eff80af178b0edc16b8bbd0114cd75
shreyash2601 <88873987+shreyash2601@users.noreply.github.com>
2024-01-07 21:59:26 +0530

    purchase fix

--- frontend/src/components/Purchases.jsx
+++ frontend/src/components/Purchases.jsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CourseItem from "./CourseItem";
 import { useNavigate, useLocation } from "react-router-dom";
+import RingLoader from "react-spinners/RingLoader";
 
 const Purchases = () => {
   const [purchasedCourses, setPurchasedCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (localStorage.getItem("token") !== "") {
-      const getPurchasedCourses = async () => {
-        try {
+    const fetchData = async () => {
+      try {
+        if (localStorage.getItem("token") !== "") {
           const response = await axios.get(
             "https://course-app-backend-gold.vercel.app/user/purchasedCourses",
             {
@@ -22,16 +24,18 @@ const Purchases = () => {
           );
           const purchases = response.data.purchasedCourses;
           setPurchasedCourses(purchases);
-        } catch (error) {
-          console.error("Error fetching courses:", error);
+        } else {
+          navigate("/login");
         }
-      };
-      getPurchasedCourses();
-    }
-    else{
-      navigate("/login");
-    }
-  }, []);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
 
   return (
     <div>
@@ -40,10 +44,14 @@ const Purchases = () => {
       </div>
       <div className="container my-3">
         <div className="row my-3">
-          {purchasedCourses.length !== 0 ? (
-            purchasedCourses.map((course) => {
-              return <CourseItem course={course} key={course._id} />;
-            })
+          {loading ? (
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <RingLoader />
+            </div>
+          ) : purchasedCourses.length !== 0 ? (
+            purchasedCourses.map((course) => (
+              <CourseItem course={course} key={course._id} />
+            ))
           ) : (
             <h5 style={{ textAlign: "center" }}>No purchases yet!</h5>
           )}
